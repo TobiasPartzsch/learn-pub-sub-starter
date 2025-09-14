20456fa454efe493e710c2dcf1c3f99de5c5c029
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2025-09-14 14:09:41 +0200

    refactor: move handlers to separate file

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -106,31 +106,6 @@ func main() {
 	}
 }
 
-func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) pubsub.Acktype {
-	return func(ps routing.PlayingState) pubsub.Acktype {
-		defer fmt.Print("> ")
-		gs.HandlePause(ps)
-		return pubsub.Ack
-	}
-}
-
-func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) pubsub.Acktype {
-	return func(mv gamelogic.ArmyMove) pubsub.Acktype {
-		defer fmt.Print("> ")
-		moveOutcome := gs.HandleMove(mv)
-		switch moveOutcome {
-		case gamelogic.MoveOutcomeSamePlayer:
-			return pubsub.NackDiscard
-		case gamelogic.MoveOutComeSafe:
-			return pubsub.Ack
-		case gamelogic.MoveOutcomeMakeWar:
-			return pubsub.Ack
-		}
-		fmt.Println("error: unknown move outcome")
-		return pubsub.NackDiscard
-	}
-}
-
 func queuePauseName(u string) string    { return routing.PauseKey + "." + u }
 func queueArmyMoveName(u string) string { return routing.ArmyMovesPrefix + "." + u }
 func bindArmyMovesPattern() string      { return routing.ArmyMovesPrefix + ".*" }
